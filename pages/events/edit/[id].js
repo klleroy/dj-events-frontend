4671414c3e09b317074e1c27807a0174c245dce9
Kevin LeRoy <kevin@kevinleroy.me>
2021-07-14 14:53:46 -0700

    Authenticated Edit, Delete & Image upload.

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -2,6 +2,7 @@ import { FaImage } from 'react-icons/fa';
 import { formatDateForInput } from 'utils/formatDate';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { parseCookies } from '@/helpers/index';
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
@@ -12,7 +13,7 @@ import ImageUpload from '@/components/ImageUpload';
 import { API_URL } from '@/config/index';
 import styles from '@/styles/Form.module.css';
 
-export default function EditEventPage({ evt }) {
+export default function EditEventPage({ evt, token }) {
 	const [values, setValues] = useState({
 		name: evt.name,
 		performers: evt.performers,
@@ -47,6 +48,7 @@ export default function EditEventPage({ evt }) {
 			method: 'PUT',
 			headers: {
 				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${token}`,
 			},
 			body: JSON.stringify(values),
 		});
@@ -173,19 +175,26 @@ export default function EditEventPage({ evt }) {
 			</div>
 
 			<Modal show={showModal} onClose={() => setShowModal(false)}>
-				<ImageUpload evtId={evt.id} imageUploaded={imageUploaded} />
+				<ImageUpload
+					evtId={evt.id}
+					imageUploaded={imageUploaded}
+					token={token}
+				/>
 			</Modal>
 		</Layout>
 	);
 }
 
-export async function getServerSideProps({ params: { id } }) {
+export async function getServerSideProps({ params: { id }, req }) {
+	const { token } = parseCookies(req);
+
 	const res = await fetch(`${API_URL}/events/${id}`);
 	const evt = await res.json();
 
 	return {
 		props: {
 			evt,
+			token,
 		},
 	};
 }
